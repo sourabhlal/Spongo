c3f55d001cdf64e7860322b58e3695f0368ecd0c
sourabhlal <sourabhlal@yahoo.co.in>
2015-02-28 21:14:55 +0100

    Setting up api query test page

--- spongo/spongoApp/views/miscellaneous.py
+++ spongo/spongoApp/views/miscellaneous.py
@@ -3,3 +3,26 @@ from django.shortcuts import render
 def home(request):
 	context = []
 	return render(request, 'homepage.html')
+
+def testing(request):
+	context = []
+	if request.method == "GET":
+		context['QueryForm'] = spongoQuery()
+		return render(request, 'testing.html', context)
+	form = spongoQuery(request.POST)
+	context['QueryForm'] = form
+	if not form.is_valid():
+		return render(request, 'testing.html', context)
+	data = form.cleaned_data
+
+	#parameters for api query:
+	#data['minimumBudget']
+	#data['maximumBudget']
+	#data['duration']
+	#data['durationUnit']
+	#data['startingDestination1']
+	#data['startingDestination2']
+	#data['startingDestination3']
+
+	#get JSON
+	return render(request, 'testing.html', context)
