138644c5845d337b2db04e9093e69275e154fec4
Nick Lee <email@nickle.es>
2015-02-28 23:19:21 +0100

    Importing skyquery file

--- spongo/spongoApp/views/miscellaneous.py
+++ spongo/spongoApp/views/miscellaneous.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from spongoApp.forms import *
+import skyquery
+#http://stackoverflow.com/questions/2349991/python-how-to-import-other-python-files
 
 def home(request):
 	context = {}
