af5e9249beb6c820f150b1071267686efc0d30dd
Nick Lee <email@nickle.es>
2015-03-01 07:18:45 +0100

    Did some refactoring, made it nicer. Stripped out Rashiq's code that was never used from his project

--- spongo/spongoApp/skyquery.py
+++ spongo/spongoApp/skyquery.py
@@ -2,9 +2,14 @@ import requests
 import time
 import urlparse
 
+__author__ = "Nick Lee"
+__copyright__ = "Copyright (C) 2015 Nick Lee"
+__license__ = "MIT License"
+
+#Reminder to remove this before open sourcing
 apikey = "ilw22874698541348193416710397562"
 
-#We can shoehorn places if need be
+#We can shoehorn places support if need be
 #Edge case, no time or sleep.
 #Removed parentID because it was bugged
 class Airport:
@@ -27,19 +32,12 @@ class Airline:
 airportCache = {}
 airlineCache = {}
 
-def addAirport(air):
-   airportCache[air.rnid] = air
-
-def addAirline(air):
-   airlineCache[air.rnid] = air
-
-#Both of these are pretty useless I think
-#Raw dict access is usable, and I'm too tired to debug
-def lookupAirportByKey(rnid):
+#Tons of accessors
+def airportObjectByKey(rnid):
    if rnid in airportCache:
       return airportCache[rnid]
 
-def lookupAirlineByKey(rnid):
+def airlineObjectByKey(rnid):
    if rnid in airportCache:
       return airportCache[rnid]
 
@@ -63,19 +61,26 @@ def airportNameByKey(rnid):
    if rnid in airportCache:
       return airportCache[rnid].iataCode
 
-#Takes JSON
+#Takes JSON input, updates the cache
 def buildAirportCache(results):
    airports = results['Places']
    for c in airports:
       #a = Airport(c['Code'], c['Id'], c['Name'], c['ParentId'], c['Type'])
       a = Airport(c['Code'], c['Id'], c['Name'], c['Type'])
-      addAirport(a)
+      addAirportToCache(a)
 
 def buildAirlineCache(results):
    carriers = results['Carriers']
    for c in carriers:
       a = Airline(c['Code'], c['DisplayCode'], c['Id'], c['ImageUrl'], c['Name'])
-      addAirline(a)
+      addAirlineToCache(a)
+
+#Also can be used as a modifier for the cache
+def addAirportToCache(air):
+   airportCache[air.rnid] = air
+
+def addAirlineToCache(air):
+   airlineCache[air.rnid] = air
 
 #Note: Postman is a nice extension
 
@@ -140,8 +145,12 @@ def getSkyScannerRoutes_raw(queryData):
 
 def getSkyScannerCosts(itinerary):
    prices = itinerary['PricingOptions']
+   priceSum = 0.0
+   count = 0
    for i in prices:
-      print itinerary['OutboundLegId'],":",u"\u20AC",i['Price']
+      priceSum += i['Price']
+      count += 1
+   return round(priceSum/count,2)
    #print itinerary['OutboundLegId'],itinerary['PricingOptions']
 
 #Prints for now BC we don't understand the data struct
@@ -158,7 +167,8 @@ def getSkyScannerSegments(itinerary):
             else:
                print "<= Flight",airlineCache[s['Carrier']].iataCode,s['FlightNumber'],"Lasting",s['Duration'],"minutes from",airportCache[s['OriginStation']].iataCode,"to",airportCache[s['DestinationStation']].iataCode
          except:
-            pass
+            return False
+      return True
 
 def getSkyScannerSegments_raw(itinerary):
    details = itinerary['BookingDetailsLink']
@@ -169,13 +179,19 @@ def getSkyScannerSegments_raw(itinerary):
 
 
 #Initiates Sessions for countries with PIA VPN endpoints
-#configured to avoid rate limit issues
+#configured to avoid rate limit issues. Experimental for use with
+#multi-market searches. If we had time, money, and more servers,
+#we'd actually operate proxies that would be used for purchasing from
+#locations abroad.
 def VPNResultSet(queryData):
    markets = {"US" : "United States", "UK" : "United Kingdom", "CH" : "Switzerland", "NL" : "Netherlands", "CA" : "Canada", "DE" : "Germany", "FR" : "France", "SE" : "Sweden", "RO" : "Romania", "HK" : "Hong Kong", "IL" : "Israel", "AU" : "Australia", "JP" : "Japan"}
    returnSet = []
    for m in markets:
       queryData['country'] = m
-      returnSet.append(getSkyScannerRoutes(queryData))
+      res = getSkyScannerRoutes(queryData)
+      returnSet.append(res)
+      buildAirportCache(res)
+      buildAirlineCache(res)
       time.sleep(6)
    return returnSet
 
@@ -191,40 +207,18 @@ def getItinerarySet(initialResults):
    r = initialResults['Itineraries']
    return r
 
-#print requests.get("http://partners.api.skyscanner.net/apiservices/reference/v1.0/countries/en-GB?apiKey="+apikey).json()
-
-#Fun fact: If the airport code doesn't exist, this app crashes
-#VPNResultSet(query)
-res = initiateSession("DE","EUR","HAM","PHL","2015-03-07","2015-03-14","Economy")
-#print airportCache
-#print airlineCache
-itins = getItinerarySet(res)
-for i in itins:
-   #print i['BookingDetailsLink']
-   #getSkyScannerCosts(i)
-   getSkyScannerSegments(i)
-   #print getSkyScannerSegments_raw(i)
-
-#print getSkyScannerRoutes_raw(query)
+#Testing code
+def test():
+   #Fun fact: If the airport code doesn't exist, this app crashes
+   #VPNResultSet(query)
+   res = initiateSession("DE","EUR","HAM","PHL","2015-03-07","2015-03-14","Economy")
+   itins = getItinerarySet(res)
+   for i in itins:
+      price = getSkyScannerCosts(i)
+      if(price!=None and getSkyScannerSegments(i)):
+         print "Trip Cost: "+u"\u20AC"+str(getSkyScannerCosts(i))+"\n"
 
 
+   #print getSkyScannerRoutes_raw(query)
 
-
-
-def get_airport(city):
-   headers = {
-      'User-Agent': 'kayakandroidphone/6.3.1',
-      'Accept-Encoding': 'gzip,deflate',
-      'Accept-Language': 'en-US',
-      'Host': 'www.kayak.com',
-      'Connection': 'Keep-Alive'
-   }
-
-   url = 'https://www.kayak.com/f/smarty?lc=en&lc_cc=US&s=1&where={city}&f=t'.format(city=city)
-
-   r = requests.get(url, headers=headers)
-   code = r.text.encode('ascii', 'ignore').split('\n')[0].split('[')[-1].split('/')[0].strip()
-   return code
-
-
-#http://partners.api.skyscanner.net/apiservices/pricing/v1.0/?apikey=ilw22874698541348193416710397562&inbounddate=2015-03-14&destinationplace=ULN&cabinclass=economy&adults=1&locale=en-GB&country=UK&outbounddate=2015-03-07&Accept=application%2Fjson&currency=GBP&originplace=IAD&locationSchema=iata&infants=0&grouppricing=false&Content-Type=application%2Fx-www-form-urlencoded&children=0
+test()
