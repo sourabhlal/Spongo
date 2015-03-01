16f3dab1c976fbf8b64259705b125c7f4697b33c
sourabhlal <sourabhlal@yahoo.co.in>
2015-03-01 10:44:04 +0100

    Lots of changes - forgot to commit earlier
    Worked on homepage, results view, spongoButton, and forms

--- spongo/spongoApp/views/miscellaneous.py
+++ spongo/spongoApp/views/miscellaneous.py
@@ -2,11 +2,60 @@ from django.shortcuts import render
 from spongoApp.forms import *
 from spongoApp.views.getComparativeData import *
 from spongoApp.views.skyquery import *
+import datetime
 
 
 def home(request):
 	context = {}
-	return render(request, 'homepage.html')
+	context['QueryForm'] = spongoQuery()
+	return render(request, 'homepage.html', context)
+
+def results(request):
+	context = {}
+	if request.method == "GET":
+		return redirect('/home')
+	
+	form = spongoQuery(request.POST)
+	context['QueryForm'] = form
+	if not form.is_valid():
+		return render(request, 'homepage.html', context)
+	data = form.cleaned_data
+	today = datetime.date.today()
+	s = today + datetime.timedelta(days=1)
+	if data['durationUnit'] == 1:
+		tripDuration = data['duration']
+	elif data['durationUnit'] == 2:
+		tripDuration = 7*int(data['duration'])
+	else:
+		tripDuration = 30*int(data['duration'])
+	f = start + datetime.timedelta(days=tripDuration)
+	
+	#parameters for api query:
+	minBudget = data['minimumBudget']
+	maxBudget = data['maximumBudget']
+	start = s.strftime('%Y-%d-%m')
+	finish = s.strftime('%Y-%d-%m')
+	startIATACode = data['startingPoint']
+
+	query = buildQueryData("DE","EUR",startIATACode,"BCN",start,finish,"Economy")
+	getSkyScannerRoutes(query)
+	res = getSkyScannerRoutes(query)
+	prices = []
+	carrier = []
+	itins = res['Itineraries']
+	for it in itins:
+		Itincost = getSkyScannerCosts(it)
+   		for i in Itincost:
+   			prices.append(i['Price'])
+   		carrier.append(getSkyScannerSegments(it))
+
+   	flightDetails = zip(prices,carrier)
+
+	context['flightDetails'] = flightDetails
+
+	return render(request, 'results.html', context)
+
+
 
 def testing(request):
 	context = {}
