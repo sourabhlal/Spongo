f3e6e2a787fa8d96559924ff3bc9fb069f103e4a
JP Breuer <jeanpaul.breuer@gmail.com>
2015-03-01 07:14:51 +0100

    lalala

--- spongo/spongoApp/static/js/baseTemplate.js
+++ spongo/spongoApp/static/js/baseTemplate.js
@@ -1,202 +1,91 @@
-var req;
+function geoFindMe() {
+  var output = document.getElementById("out");
 
-function toggle_form_visibility(id1) {
-   var item = document.getElementById(id1);
-   if(item.style.display == 'none'){
-      	item.style.display = 'inline';
-      }
-   else{
-   	  	item.style.display = 'none';
-   }   
-   console.log(item);
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+    var img = new Image();
+    img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    output.appendChild(img);
+  };
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  };
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
 }
 
-function requestNotifications(){
-	if (window.XMLHttpRequest) {
-	   req = new XMLHttpRequest();
-	} else {
-	    req = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	resetThisTimer = 1;
-	req.onreadystatechange = updateNotifications;
-	req.open("GET", "/userNotifications", true);
-	req.send(); 
-}
-window.setInterval(requestNotifications,5000);
+function prompt(window, pref, message, callback) {
+    let branch = Components.classes["@mozilla.org/preferences-service;1"]
+                           .getService(Components.interfaces.nsIPrefBranch);
 
-function updateNotifications() {
-	if (req.readyState != 4 || req.status != 200) {
-        return;
-    }   
- 
-    var list = document.getElementById("listOfNotifications");
-    var badge = document.getElementById("counter");
-    var electric = document.getElementById("counterGlyph");
-    while (list.hasChildNodes()) {
-        list.removeChild(list.firstChild);
+    if (branch.getPrefType(pref) === branch.PREF_STRING) {
+        switch (branch.getCharPref(pref)) {
+        case "always":
+            return callback(true);
+        case "never":
+            return callback(false);
+        }
     }
 
-    var items = JSON.parse(req.responseText);
-    badge.innerText = items.length
-    if (items.length == 0){
-    	var newItem = document.createElement("li");
-        newItem.innerHTML = "<a href=\"#\">No new notifications<button type=\"submit\" class=\"btn btn-default btn-sm\"><span class=\"glyphicon glyphicon-refresh\"></span></button></a>"
-        list.appendChild(newItem);
+    let done = false;
+
+    function remember(value, result) {
+        return function() {
+            done = true;
+            branch.setCharPref(pref, value);
+            callback(result);
+        }
     }
-    else{
-    	electric.className += " yellow";
-    	for (var i = 0; i < items.length; ++i) {
-        	var id = items[i]["pk"];
-        	console.log(id);
-        	var title = items[i]["fields"]["title"];
-  
-        	var newItem = document.createElement("li");
-        	newItem.innerHTML = "<a href=\"/profile/notification/"+id+"\">"+title+"</a>";
-        	list.appendChild(newItem);
-    	}
-    }
+
+    let self = window.PopupNotifications.show(
+        window.gBrowser.selectedBrowser,
+        "geolocation",
+        message,
+        "geo-notification-icon",
+        {
+            label: "Share Location",
+            accessKey: "S",
+            callback: function(notification) {
+                done = true;
+                callback(true);
+            }
+        }, [
+            {
+                label: "Always Share",
+                accessKey: "A",
+                callback: remember("always", true)
+            },
+            {
+                label: "Never Share",
+                accessKey: "N",
+                callback: remember("never", false)
+            }
+        ], {
+            eventCallback: function(event) {
+                if (event === "dismissed") {
+                    if (!done) callback(false);
+                    done = true;
+                    window.PopupNotifications.remove(self);
+                }
+            },
+            persistWhileVisible: true
+        });
 }
 
-$(document).ready(function() {
-	if (window.XMLHttpRequest) {
-	    req = new XMLHttpRequest();
-	} else {
-	    req = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	resetThisTimer = 1;
-	req.onreadystatechange = updateNotifications;
-	req.open("GET", "/updateStatuses", true);
-	req.send(); 
-
-	var hideWidth = '-170px'; //width that will be hidden
-	var collapsibleEl = $('.collapsible'); //collapsible element
-	var buttonEl =  $(".collapsible #main"); //big button 
-	var winHeight = $(window).height()*.93; 
-	var expanded = false;
-
-	collapsibleEl.css({'position':'fixed','right': hideWidth,'height': winHeight});
-	buttonEl.css({'height': winHeight-10});
-
-	$(buttonEl).click(function()
-	{
-	    if(expanded)
-	    {
-	        $(this).parent().animate({right: hideWidth}, 300 );
-	        $(this).html('&laquo;'); //change text of button
-	        expanded = false;
-	    }else{
-	        $(this).parent().animate({right: "0"}, 300 ); 
-	        $(this).html('&raquo;'); //change text of button
-	        expanded = true;
-	    }
-	});
-
-	$( window ).resize(function() {
-		winHeight = $(window).height()*.93;
-	});
-
-	var visButton = $('.visButton');
-	var priceButton= $('.priceButton');
-	
-	$(visButton).click(function()
-	{
-		var bid_id = this.id.split("_")[1];
-		priceButton_id = "#priceButton_" + bid_id
-		priceSubmit_id = "#priceSubmit_" + bid_id
-
-		var thisPriceButton = $(priceButton_id);		
-		var thisPriceSubmit = $(priceSubmit_id);
-
-		$(this).css({'display':'none'});
-		thisPriceSubmit.css({'display':'block'});
-		thisPriceButton.css({'display':'block'});
-	});
-
-	$(priceButton).click(function()
-	{
-		var bid_id = this.id.split("_")[1];
-		visButton_id = "#visButton_" + bid_id
-		priceSubmit_id = "#priceSubmit_" + bid_id
-
-		var thisVisButton = $(visButton_id);		
-		var thisPriceSubmit = $(priceSubmit_id);
-
-		thisVisButton.css({'display':'block'});
-		$(this).css({'display':'none'});
-		thisPriceSubmit.css({'display':'none'});
-
-		numField_id = "#priceInput_"+bid_id;
-		var new_price = $(numField_id).val();		
-
-		priceRequest(bid_id,new_price);
-	});
-});
-
-var priceReq;
-function priceRequest(bid_id,new_price) {
-	if(window.XMLHttpRequest){
-		priceReq = new XMLHttpRequest();
-	}
-	else {
-		priceReq = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-
-	priceReq.onreadystatechange = priceResponse;
-	priceReq.open("GET", "/updateBidPrice/"+bid_id+"/"+new_price, true);
-	priceReq.send();
-}
-
-function priceResponse(){
-	if (priceReq.readyState != 4 || priceReq.status != 200){
-		return;
-	}
-
-	var bidData = JSON.parse(priceReq.responseText)
-	for(var i = 0; i < bidData.length; i++){
-		var b_id = bidData[i]["pk"];
-		var p_id = "price_" + b_id;
-		var price = parseInt(bidData[i]["fields"]["price"]);
-
-
-		$('#'+p_id).text('Your Offer: $'+price)
-	}
-}
-
-
-var trackReq;
-function trackerRequest() {
-	if(window.XMLHttpRequest){
-		trackReq = new XMLHttpRequest();
-	}
-	else {
-		trackReq = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-
-	trackReq.onreadystatechange = trackerResponse;
-	trackReq.open("GET", "/userBidsTrack", true);
-	trackReq.send();
-}
-
-window.setInterval(trackerRequest,1000);
-
-function trackerResponse(){
-	if (trackReq.readyState != 4 || trackReq.status != 200){
-		return;
-	}
-
-	var bidData = JSON.parse(trackReq.responseText)
-	for(var i = 0; i < bidData.length; i++){
-		var b_id = bidData[i]["pk"];
-		var track_id = "tracker_" + b_id;
-		var status = parseInt(bidData[i]["fields"]["status"]);
-
-		if(status == 1 || status == 0){
-			var t = document.getElementById(track_id).parentNode.parentNode;
-			t.style.background = "rgba(113,188,120,0.65)";
-		}
-		else{
-			var t = document.getElementById(track_id).parentNode.parentNode;
-			t.style.background = "rgba(120, 120, 120,0.85)";
-		}
-	}
-}
+prompt(window,
+       "extensions.foo-addon.allowGeolocation",
+       "Foo Add-on wants to know your location.",
+       function callback(allowed) { alert(allowed); });
